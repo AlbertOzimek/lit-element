4a82b2bf2020ae2bcfc4a7fb0233161899d45c2b
Albert Ozimek <albert.ozimek@gmail.com>
2019-05-05 09:17:48 +0200

    adding todos

--- src/views/todo-view.js
+++ src/views/todo-view.js
@@ -1,9 +1,86 @@
 import {LitElement, html} from '@polymer/lit-element';
+import '@vaadin/vaadin-text-field';
+import '@vaadin/vaadin-button';
+import '@vaadin/vaadin-checkbox';
+import '@vaadin/vaadin-radio-button/vaadin-radio-button';
+import '@vaadin/vaadin-radio-button/vaadin-radio-group';
+
+const VisibilityFilters = {
+    SHOW_ALL: 'All',
+    SHOW_ACTIVE: 'Active',
+    SHOW_COMPLETED: 'Completed'
+};
 
 class TodoView extends LitElement {
-   render() {
+
+    constructor() {
+        super();
+        this.todos = [];
+        this.filter = VisibilityFilters.SHOW_ALL;
+        this.task = '';
+    }
+
+    static get properties() {
+      return {
+          todos: { type: Array},
+          filter: { type: String},
+          task: { type: String}
+      }
+    }
+
+
+  updateTask(e) {
+     this.task = e.target.value;
+  }
+
+    addTodo() {
+        if (this.task) {
+            this.todos = [...this.todos, {
+                task: this.task,
+                complete: false
+            }];
+            this.task = '';
+        }
+    }
+
+    shortcutListener(e) {
+        if (e.key === 'Enter') {
+            this.addTodo();
+        }
+    }
+
+
+    render() {
        return html`
-         <p>Hello World</p>
+        <div class="input-layout"
+          @keyup="${this.shortcutListener}"> 
+          <vaadin-text-field
+            placeholder="Task"
+            value="${this.task}" 
+            @change="${this.updateTask}"> 
+          </vaadin-text-field>
+        
+          <vaadin-button
+            theme="primary"
+            @click="${this.addTodo}"> 
+              Add Todo
+          </vaadin-button>
+        </div>
+        
+        
+        <div class="todo-list">
+         ${this.todos.map(todo => html`
+                <div class="todo-item">
+                      <vaadin-checkbox
+                        ?checked="${todo.complete}" 
+                        @change="${ e => this.updateTodoStatus(todo, e.target.checked)}"> 
+                        ${todo.task}
+                      </vaadin-checkbox>
+                </div>
+         `)}
+        </div>
+        
+        
        `;
    } 
 }
